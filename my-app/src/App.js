bdea0256122ccf7807ac559a1f39c597abc1128d
Ajorin <peterwgwhitehead@btinternet.com>
2019-02-21 15:39:22 +0000

    Create Instruction Component + Button to show Article

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,16 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 import ArticleMain from './ArticleMain.js'
 import Imports from './Imports.js'
+import Instructions from './Instructions.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showArticleBox: false
+    }
+  }
+
+  showArticle() {
+    this.setState({showArticleBox: true});
+  }
 
   render() {
     return (
       <div className="App">
         <Imports />
         <h1>Article Ranker</h1>
-        <ArticleMain text="hello" />
-        <button className="btn" type="button">button</button>
+        <Instructions />
+        <ArticleMain text="hello" show={this.state.showArticleBox} />
+        <button className="btn" type="button" onClick={this.showArticle.bind(this)} >button</button>
       </div>
     );
   }
---
+++ my-app/src/Instructions.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Instructions extends Component {
+  constructor(props) {
+  	super(props);
+  }
+
+  render(){
+  	return(
+  	  <div>
+  	    <p>Instructions</p>
+  	  </div>
+  	);
+  }
+}
+
+export default Instructions;
