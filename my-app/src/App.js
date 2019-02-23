7392fac87379a36818312c3c2871d85b8510754d
Ajorin <peterwgwhitehead@btinternet.com>
2019-02-23 23:55:25 +0000

    Create Article Rendering Methods

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,12 @@ import ArticleMain from './ArticleMain.js'
 import Imports from './Imports.js'
 import Instructions from './Instructions.js'
 import axios from 'axios'
+import article1 from './data/article-1.json';
+import article2 from './data/article-2.json';
+import article3 from './data/article-3.json';
+import article4 from './data/article-4.json';
+import article5 from './data/article-5.json';
+
 
 class App extends Component {
 
@@ -12,13 +18,17 @@ class App extends Component {
     super(props);
     this.state = {
       showArticleBox: false,
-      articles: []
+      artcileSelected: '',
+      articles: [article1, article2, article3, article4, article5]
     }
   }
 
-   componentDidMount() {
 
-   }
+  readArticle() {
+    let article = JSON.parse(JSON.stringify(article1));
+    console.log(article);
+    this.setState({articl1: article});
+  }
 
   showArticle() {
     this.setState({showArticleBox: true});
@@ -31,15 +41,20 @@ class App extends Component {
     }).catch(function(err) {
       console.log(err); //Do something with err
     });
+    //Now Stubbing the Get Request
+    let article = JSON.parse(JSON.stringify(article5));
+    console.log(article);
+    console.log(this.state.articles);
+    this.setState({articleSelected: article});
   }
 
   render() {
     return (
       <div className="App">
-        <Imports />
         <h1>Article Ranker</h1>
+        <Imports />
         <Instructions />
-        <ArticleMain text="hello" show={this.state.showArticleBox} />
+        <ArticleMain article={this.state.articleSelected} show={this.state.showArticleBox}  />
         <button className="btn" type="button" onClick={this.showArticle.bind(this)} >button</button>
       </div>
     );
--- my-app/src/ArticleMain.js
+++ my-app/src/ArticleMain.js
@@ -7,21 +7,84 @@ class ArticleMain extends Component {
   }
 
   readArticle() {
-    //get article
+    if (this.props.artcle === '' || typeof this.props.article === "undefined") {
+    } else {
+        console.log(this.props.article.body);
+        console.log("length: " + this.props.article.length);
+    }
+    let articleData = this.props.article;
+    console.log(articleData);
   }
 
-  componentDidMount() {
-    //get request goes here
+  createParagraph(text) {
+    return (<p>{text}</p>)
+  }
+
+  createImage(model) {
+    return (<img src={model.url} alt={model.altText} height={model.height} width={model.width} />);
+  }
+
+  createList(items) {
+    return (
+      <ul> 
+        {this.createListItems(items)}
+      </ul>
+    )
+  }
+
+  createListItems(items) {
+    let listItems = [];
+    for (let i=0; i< items.length; i++) {
+      listItems.push(<li>{items[i]}</li>);
+    }
+    return listItems
+  }
+
+  createHeading(text) {
+    console.log("Heading: " + text)
+    return (<h2>{text}</h2>);
+  }
+
+
+  renderArticle() {
+    let article = this.props.article;
+    let articleComponents = [];
+    for (let i=0; i < article.body.length; i++) {
+      console.log(article.body[i].type);
+      if (article.body[i].type === "heading") {
+        articleComponents.push(this.createHeading(article.body[i].model.text));
+      }
+        if (article.body[i].type === "image") {
+        articleComponents.push(this.createImage(article.body[i].model));
+      }
+      if (article.body[i].type === "paragraph") {
+        articleComponents.push(this.createParagraph(article.body[i].model.text));
+      }
+      if (article.body[i].type === "list") {
+        articleComponents.push(this.createList(article.body[i].model.items));
+      }
+    }
+    return (articleComponents);
+  }
+
+  renderArticleTitle() {
+    console.log("title:" + this.props.article.title);
+    return (
+      <h1>{this.props.article.title}</h1>
+    )
   }
 
   render() {
+    console.log(this.props.show);
+    console.log(this.props.article);
     if (this.props.show === false) {
       return null
     }
 
   	return(
   	  <div>
-  	  	<p>{this.props.text}</p>
+      {this.renderArticleTitle()}
+      {this.renderArticle()}
   	  </div>
   	)
   }
